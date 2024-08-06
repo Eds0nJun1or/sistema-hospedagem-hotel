e6377f4707e7759c23b8c360af44637e1891741a
Eds0nJun1or <oedsongoncalves@gmail.com>
2024-08-06 16:28:10 -0300

    Adicionar arquivos de projeto.

---
+++ sistema-hospedagem-hotel/Models/Reserva.cs
@@ -0,0 +1,50 @@
+﻿namespace sistema_hospedagem_hotel.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            if (diasReservados <= 0)
+                throw new ArgumentException("O número de dias reservados deve ser um número positivo.");
+
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite ?? throw new ArgumentException("A reserva deve estar associada a uma suíte.");
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes == null || hospedes.Count == 0)
+                throw new ArgumentException("A reserva deve ter pelo menos um hóspede.");
+
+            if (hospedes.Count > Suite.Capacidade)
+                throw new ArgumentException("O número de hóspedes excede a capacidade da suíte.");
+
+            Hospedes = hospedes;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m; // Aplicando desconto de 10%
+            }
+            return valor;
+        }
+    }
+}
---
+++ sistema-hospedagem-hotel/Models/Suite.cs
@@ -0,0 +1,24 @@
+﻿namespace sistema_hospedagem_hotel.Models
+{
+    public class Suite
+    {
+        public Suite() { }
+
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            if (capacidade <= 0)
+                throw new ArgumentException("A capacidade da suíte deve ser um número positivo.");
+
+            if (valorDiaria <= 0)
+                throw new ArgumentException("O valor da diária deve ser um valor positivo.");
+
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+    }
+}
